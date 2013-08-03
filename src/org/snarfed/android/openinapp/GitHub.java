a295e106a6c128acbea01c5776f01414272278fb
Ryan Barrett <git@ryanb.org>
2013-08-03 14:30:40 -0700

    finish github first pass

--- src/org/snarfed/android/openinapp/GitHub.java
+++ src/org/snarfed/android/openinapp/GitHub.java
@@ -11,11 +11,17 @@ import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 
-// Test command line: adb -d shell am start -d [link]
-// https://github.com/snarfed/open-in-app
-// https://github.com/snarfed
-
+// The GitHub app's UriLauncherActivity claims it can handle all github.com and
+// gist.github.com URLs, so just pass them on untouched.
 // https://github.com/github/android/blob/master/app/AndroidManifest.xml
+
+// It doesn't actually handle some of them though:
+// Files: http://github.com/snarfed/open-in-app/blob/master/build.xml
+// Issue browser: http://github.com/snarfed/facebook-atom/issues
+// Pull request browser: http://github.com/rogerhu/mockfacebook/pulls
+// All gist URLs:
+//  https://gist.github.com/JakeWharton
+//  https://gist.github.com/JakeWharton/6002797
 public class GitHub extends Activity {
   private static final String TAG = "GitHub";
 
@@ -30,10 +36,11 @@ public class GitHub extends Activity {
       return;
     }
 
-    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    // Strip www. from host if it's there.
+    Uri newUri = Uri.parse("https://github.com" + uri.getPath());
+    Intent intent = new Intent(Intent.ACTION_VIEW, newUri);
     Log.i(TAG, "Redirecting " + uri + " to package com.github.mobile");
     intent.setPackage("com.github.mobile");
-
     startActivity(intent);
     finish();
   }
